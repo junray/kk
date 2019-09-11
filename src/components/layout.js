b34f197462bf1670965b48cbfa1cc2c128a077e1
junray <marco.digennaro@feries.it>
2019-09-11 11:25:27 +0200

    favicon

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,25 +2,19 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
+import favicon from '../images/favicon.png'
 
 import '../assets/scss/main.scss'
 
 const Layout = ({ children, location }) => {
-
-  let content;
+  let content
 
   if (location && location.pathname === '/') {
-    content = (
-      <div>
-        {children}
-      </div>
-    )
+    content = <div>{children}</div>
   } else {
     content = (
       <div id="wrapper" className="page">
-        <div>
-          {children}
-        </div>
+        <div>{children}</div>
       </div>
     )
   }
@@ -41,8 +35,23 @@ const Layout = ({ children, location }) => {
           <Helmet
             title={data.site.siteMetadata.title}
             meta={[
-              { name: 'description', content: 'Sample' },
-              { name: 'keywords', content: 'sample, something' },
+              { name: 'description', content: 'klippa kloppa website' },
+              { name: 'keywords', content: 'music, art' },
+            ]}
+            link={[
+              {
+                rel: 'icon',
+                type: 'image/png',
+                sizes: '16x16',
+                href: `${favicon}`,
+              },
+              {
+                rel: 'icon',
+                type: 'image/png',
+                sizes: '32x32',
+                href: `${favicon}`,
+              },
+              { rel: 'shortcut icon', type: 'image/png', href: `${favicon}` },
             ]}
           >
             <html lang="en" />
