3bc6bb2f864335f819c61118da9c8d55a2c2ad7a
junray <marco.digennaro@feries.it>
2019-09-11 11:15:41 +0200

    startup

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,42 +1,159 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-
-import pic01 from '../images/pic01.jpg'
-import pic02 from '../images/pic02.jpg'
-import pic03 from '../images/pic03.jpg'
+import Video from '../components/video'
+import social from '../images/social.jpg'
+import megaphone from '../images/megaphone.jpg'
+import blowup from '../images/blowup.pdf'
+import buscadero from '../images/buscadero.pdf'
 
 class Main extends React.Component {
   render() {
+    let close = (
+      <div
+        className="close"
+        onClick={() => {
+          this.props.onCloseArticle()
+        }}
+      ></div>
+    )
 
-    let close = <div className="close" onClick={() => {this.props.onCloseArticle()}}></div>
+    console.log(this.props)
 
     return (
-      <div ref={this.props.setWrapperRef} id="main" style={this.props.timeout ? {display: 'flex'} : {display: 'none'}}>
+      <div
+        ref={this.props.setWrapperRef}
+        id="main"
+        style={this.props.timeout ? { display: 'flex' } : { display: 'none' }}
+      >
+        <article
+          id="video"
+          className={`${this.props.article === 'video' ? 'active' : ''} ${
+            this.props.articleTimeout ? 'timeout' : ''
+          }`}
+          style={{ display: 'none' }}
+        >
+          <h2 className="major">Video</h2>
 
-        <article id="intro" className={`${this.props.article === 'intro' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
-          <h2 className="major">Intro</h2>
-          <span className="image main"><img src={pic01} alt="" /></span>
-          <p>Aenean ornare velit lacus, ac varius enim ullamcorper eu. Proin aliquam facilisis ante interdum congue. Integer mollis, nisl amet convallis, porttitor magna ullamcorper, amet egestas mauris. Ut magna finibus nisi nec lacinia. Nam maximus erat id euismod egestas. By the way, check out my <a href="#work">awesome work</a>.</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dapibus rutrum facilisis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam tristique libero eu nibh porttitor fermentum. Nullam venenatis erat id vehicula viverra. Nunc ultrices eros ut ultricies condimentum. Mauris risus lacus, blandit sit amet venenatis non, bibendum vitae dolor. Nunc lorem mauris, fringilla in aliquam at, euismod in lectus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In non lorem sit amet elit placerat maximus. Pellentesque aliquam maximus risus, vel sed vehicula.</p>
+          <Video
+            videoSrcURL="https://www.youtube.com/embed/tapkzNyruls"
+            videoTitle={'Cinghiali'}
+            videoSubTitle="Scritto e dirretto da Davide Maldi"
+            ratio="16-9"
+          />
+
+          <br />
+          <br />
+          <Video
+            videoSrcURL="https://www.youtube.com/embed/mbf-GHKC2e0"
+            videoTitle="Bach"
+            videoSubTitle="Scritto e dirretto da Flavio Scutti"
+            ratio="16-9"
+          />
+
           {close}
         </article>
 
-        <article id="work" className={`${this.props.article === 'work' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
-          <h2 className="major">Work</h2>
-          <span className="image main"><img src={pic02} alt="" /></span>
-          <p>Adipiscing magna sed dolor elit. Praesent eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent urna nisi, fringila lorem et vehicula lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices.</p>
-          <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus pharetra. Pellentesque condimentum sem. In efficitur ligula tate urna. Maecenas laoreet massa vel lacinia pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus amet feugiat tempus.</p>
+        <article
+          id="stampa"
+          className={`${this.props.article === 'stampa' ? 'active' : ''} ${
+            this.props.articleTimeout ? 'timeout' : ''
+          }`}
+          style={{ display: 'none' }}
+        >
+          <h2 className="major">Stampa</h2>
+          <span className="image main">
+            <img style={{ opacity: 0.9 }} src={megaphone} alt="" />
+          </span>
+          <h2>Web magazines</h2>
+          <ul>
+            <li>
+              <a
+                href="https://sentireascoltare.com/recensioni/klippa-kloppa-liberty/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Sentire ascoltare
+              </a>
+            </li>
+          </ul>
+          <h2>Paper magazines</h2>
+          <ul>
+            <li>
+              <a
+                href="http://www.blowupmagazine.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Blow up
+              </a>
+              :{' '}
+              <a rel="noopener noreferrer" target="_blank" href={blowup}>
+                scarica il pdf
+              </a>
+            </li>
+            <li>
+              <a rel="noopener noreferrer" href="http://www.buscadero.com/m">
+                Buscadero
+              </a>
+              :{' '}
+              <a rel="noopener noreferrer" target="_blank" href={buscadero}>
+                scarica il pdf
+              </a>
+            </li>
+          </ul>
           {close}
         </article>
 
-        <article id="about" className={`${this.props.article === 'about' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
-          <h2 className="major">About</h2>
-          <span className="image main"><img src={pic03} alt="" /></span>
-          <p>Lorem ipsum dolor sit amet, consectetur et adipiscing elit. Praesent eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent urna nisi, fringila lorem et vehicula lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices. Aliquam libero et malesuada fames ac ante ipsum primis in faucibus. Cras viverra ligula sit amet ex mollis mattis lorem ipsum dolor sit amet.</p>
+        <article
+          id="social"
+          className={`${this.props.article === 'social' ? 'active' : ''} ${
+            this.props.articleTimeout ? 'timeout' : ''
+          }`}
+          style={{ display: 'none' }}
+        >
+          <h2 className="major">Social</h2>
+          <span className="image main">
+            <img src={social} alt="" />
+          </span>
+          <p>
+            Anche disponibile su:&nbsp;
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://open.spotify.com/album/67hZedDP3rYe3CogTA0pJD
+"
+            >
+              spotify
+            </a>
+          </p>
+          <p className="m-bottom4x">
+            Seguici su&nbsp;
+            <a
+              href="https://www.facebook.com/klippakloppa/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              facebook
+            </a>
+            &nbsp;e su&nbsp;
+            <a
+              href="https://www.instagram.com/klippa_kloppa/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              instagram
+            </a>
+          </p>
           {close}
         </article>
 
-        <article id="contact" className={`${this.props.article === 'contact' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
+        <article
+          id="contact"
+          className={`${this.props.article === 'contact' ? 'active' : ''} ${
+            this.props.articleTimeout ? 'timeout' : ''
+          }`}
+          style={{ display: 'none' }}
+        >
           <h2 className="major">Contact</h2>
           <form method="post" action="#">
             <div className="field half first">
@@ -52,19 +169,17 @@ class Main extends React.Component {
               <textarea name="message" id="message" rows="4"></textarea>
             </div>
             <ul className="actions">
-              <li><input type="submit" value="Send Message" className="special" /></li>
-              <li><input type="reset" value="Reset" /></li>
+              <li>
+                <input type="submit" value="Send Message" className="special" />
+              </li>
+              <li>
+                <input type="reset" value="Reset" />
+              </li>
             </ul>
           </form>
-          <ul className="icons">
-            <li><a href="#" className="icon fa-twitter"><span className="label">Twitter</span></a></li>
-            <li><a href="#" className="icon fa-facebook"><span className="label">Facebook</span></a></li>
-            <li><a href="#" className="icon fa-instagram"><span className="label">Instagram</span></a></li>
-            <li><a href="#" className="icon fa-github"><span className="label">GitHub</span></a></li>
-          </ul>
+
           {close}
         </article>
-
       </div>
     )
   }
---
+++ src/components/video.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Video = ({ videoSrcURL, videoTitle, videoSubTitle, ratio, ...props }) => {
+  return (
+    <React.Fragment>
+      <h2 style={{ marginBottom: '10px' }}>{videoTitle}</h2>
+      <p style={{ marginTop: 0 }}>{videoSubTitle}</p>
+      <div className={`video r${ratio}`}>
+        <iframe
+          src={videoSrcURL}
+          title={videoTitle}
+          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+          frameBorder="0"
+          webkitallowfullscreen="true"
+          mozallowfullscreen="true"
+          allowFullScreen
+          width="100%"
+          height="100%"
+        />
+      </div>
+    </React.Fragment>
+  )
+}
+export default Video
